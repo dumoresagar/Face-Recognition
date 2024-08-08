3d11952f436f1cbc769d444ab66bb0f6997c58b4
dumoresagar <dumoresagar@gmail.com>
2024-08-08 17:31:14 +0530

    second

--- backend/restfull_apis/version_0/authentication/urls.py
+++ backend/restfull_apis/version_0/authentication/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from .api import (
-    ProtoTypeLookUp, CustomLoginAPIView, RegisterAPIView, CustomPasswordChangeView, CustomPasswordResetView,
-    CustomPasswordResetConfirmView,UpdateUserAPIView,RecognizeFaceAPIView
+    ProtoTypeLookUp,RecognizeFaceAPIView
 )
 
 
@@ -9,17 +8,6 @@ app_name = "authentication"
 
 urlpatterns = [
     path('init/', ProtoTypeLookUp.as_view(), name='init-rest'),
-
-    path('login/', CustomLoginAPIView.as_view(), name='login-rest'),
-    path('register/', RegisterAPIView.as_view(), name='register-rest'),
-
-    path('update-user/<int:pk>/', UpdateUserAPIView.as_view(), name='register-rest'),
-
-
-    path('change-password/', CustomPasswordChangeView.as_view(), name='change-password-reset'),
-    path('forgot-password/', CustomPasswordResetView.as_view(), name='forgot-password-rest'),
-    path('forgot-password/confirm/', CustomPasswordResetConfirmView.as_view(), name='forgot-password-confirm-rest'),
-    
 
     path('recognize/', RecognizeFaceAPIView.as_view(), name='recognize_face'),
 
